54f703f1cc4ebb55f6be3f55b621935d30c77323
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-01-19 18:57:01 +0530

    Add listings and realors then add  custom css to admin page

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Listing
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title' , 'is_Published' , 'price' , 'state', 'list_data' , 'realtor')
+    list_display_links=('id','title')
+    list_filter=('realtor',)
+    list_editable=('is_Published',)
+    search_fields=('title','description','address','city','state','zipcode')
+admin.site.register(Listing,ListingAdmin)
 
 # Register your models here.
