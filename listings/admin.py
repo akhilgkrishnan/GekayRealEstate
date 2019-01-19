b2b1efce9aeecfbad9217ee27f9bdd6285e37990
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-01-19 19:13:33 +0530

    Add search in admin listing

--- listings/admin.py
+++ listings/admin.py
@@ -6,7 +6,9 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links=('id','title')
     list_filter=('realtor',)
     list_editable=('is_Published',)
-    search_fields=('title','description','address','city','state','zipcode')
+    search_fields=('title','description','address','city','state','zipcode','price')
+    list_per_page=25
 admin.site.register(Listing,ListingAdmin)
+
 
 # Register your models here.
