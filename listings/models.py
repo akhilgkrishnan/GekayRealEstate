e21eb2070449fef9e8edaa80e6d1d8a28fb152f5
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-01-20 09:41:24 +0530

    Add dynamic listing contents

--- listings/models.py
+++ listings/models.py
@@ -16,7 +16,7 @@ class Listing(models.Model):
     garage =models.IntegerField(default=0)
     sqft=models.IntegerField()
     lot_size=models.DecimalField(max_digits=5,decimal_places=1)
-    photo_main =models.ImageField(upload_to='photos/%Y/%m?%d/')
+    photo_main = models.ImageField(upload_to='photos/%Y/%m?%d/')
     photo_1 =models.ImageField(upload_to='photos/%Y/%m?%d/',blank=True)
     photo_2 =models.ImageField(upload_to='photos/%Y/%m?%d/',blank=True)
     photo_3 =models.ImageField(upload_to='photos/%Y/%m?%d/',blank=True)
