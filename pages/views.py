9a4846da3670e02252c599a794ac42693dfd3eae
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-01-21 16:41:24 +0530

    Add dynamic contents in about page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_data').filter(is_Published=True)[:3]
@@ -10,6 +11,16 @@ def index(request):
     }
     return render(request, 'pages/index.html', context)
 def about(request):
-    return render(request, 'pages/about.html')    
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    #Get Mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors' : realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html',context)   
+    
+    
 
 # Create your views here.
