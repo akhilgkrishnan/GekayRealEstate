5f4a4622781267ff95680b4a86f5d49739086261
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-02-15 21:45:57 +0530

    update search table in home

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.choice import price_choices,bedroom_choices,state_choices                                                                                                                 
 from listings.models import Listing
 from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_data').filter(is_Published=True)[:3]
     context = {
-        'listings': listings
-    }
+        'listings': listings,
+        'state_choices':state_choices,    }
+        'bedroom_choice':bedroom_choices,
+        'price_choice':price_choices
     return render(request, 'pages/index.html', context)
 def about(request):
     # Get all realtors
