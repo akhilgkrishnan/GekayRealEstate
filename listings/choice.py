d76aeea2ff91123dcd53ed660e092df0762ffefc
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-02-16 23:01:22 +0530

    a

--- listings/choice.py
+++ listings/choice.py
@@ -36,3 +36,16 @@ state_choices ={
 'LD':'Lakshadweep'
 'PU':'Puducherry'
 }
+
+bedroom_choices={
+    '1':1
+    '2':2
+    '3':3
+    '4':4
+    '5':5
+    '6':6
+    '7':7
+    '8':8
+    '9':9
+    '10':10
+}
