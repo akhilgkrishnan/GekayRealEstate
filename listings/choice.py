da60742b299e6f258127b0e786ee217fad0b825e
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-02-17 22:40:05 +0530

    add  more choices

--- listings/choice.py
+++ listings/choice.py
@@ -1,51 +1,62 @@
 state_choices ={
-'AP':'Andra Pradesh'
-'AR':'Arunachal Pradesh'
-'AS':'Assam'
-'BH':'Bihar'
-'CH':'Chhattisgarh'
-'GO':'Goa'
-'GJ':'Gujarat'
-'HY':'Haryana'
-'HP':'Himachal Pradesh'
-'JK':'Jammu & Kasmir'
-'JH':'Jharkand'
-'KA':'Karnataka'
-'KL':'Kerala'
-'MP':'Madhya Pradesh'
-'MH':'Maharastra'
-'MN':'Manipur'
-'MG':'Meghalaya'
-'MZ':'Mizoram'
-'NG':'Nagaland'
-'OD':'Odisha'
-'PJ':'Punjab'
-'RJ':'Rajasthan'
-'SI':'Sikkim'
-'TN':'Tamil Nadu'
-'TE':'Telangana'
-'TR':'Tripura'
-'UP':'Uttar Pradesh'
-'UT':'Uttarakhand'
-'WB':'West Bengal'
-'AN':'Andaman Nicobar'
-'CG':'Chandigarh'
-'DN':'Dadra and Nagar Haveli'
-'DD':'Daman and Diu'
-'DL':'Delhi'
-'LD':'Lakshadweep'
+'AP':'Andra Pradesh',
+'AR':'Arunachal Pradesh',
+'AS':'Assam',
+'BH':'Bihar',
+'CH':'Chhattisgarh',
+'GO':'Goa',
+'GJ':'Gujarat',
+'HY':'Haryana',
+'HP':'Himachal Pradesh',
+'JK':'Jammu & Kasmir',
+'JH':'Jharkand',
+'KA':'Karnataka',
+'KL':'Kerala',
+'MP':'Madhya Pradesh',
+'MH':'Maharastra',
+'MN':'Manipur',
+'MG':'Meghalaya',
+'MZ':'Mizoram',
+'NG':'Nagaland',
+'OD':'Odisha',
+'PJ':'Punjab',
+'RJ':'Rajasthan',
+'SI':'Sikkim',
+'TN':'Tamil Nadu',
+'TE':'Telangana',
+'TR':'Tripura',
+'UP':'Uttar Pradesh',
+'UT':'Uttarakhand',
+'WB':'West Bengal',
+'AN':'Andaman Nicobar',
+'CG':'Chandigarh',
+'DN':'Dadra and Nagar Haveli',
+'DD':'Daman and Diu',
+'DL':'Delhi',
+'LD':'Lakshadweep',
 'PU':'Puducherry'
 }
 
 bedroom_choices={
-    '1':1
-    '2':2
-    '3':3
-    '4':4
-    '5':5
-    '6':6
-    '7':7
-    '8':8
-    '9':9
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
     '10':10
+}
+price_choices={
+'100000':'₹100,000',
+'200000':'₹200,000',
+'300000':'₹300,000',
+'400000':'₹400,000',
+'500000':'₹500,000',
+'600000':'₹600,000',
+'700000':'₹700,000',
+'800000':'₹800,000',
+'900000':'₹900,000'
 }
