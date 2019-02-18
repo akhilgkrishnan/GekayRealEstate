0f13e5bcaefb9eb83aca3aaa325bdd1ff405f3bb
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-02-18 20:38:30 +0530

    add dynamic content for search

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from .choice import price_choices,bedroom_choices,state_choices                                                                                                                 
+
 from .models import Listing
 
 def index(request):
@@ -19,5 +21,38 @@ def listing(request, listing_id):
     }
     return render(request, 'listings/listing.html',context)
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_data')
+    #keyword
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list=queryset_list.filter(description__icontains=keywords)
+    #city
+    if 'city' in request.GET:
+        city= request.GET['city']
+        if city:
+            queryset_list=queryset_list.filter(city__iexact=city)   
+    #state
+    if 'state' in request.GET:
+        state= request.GET['state']
+        if state:
+            queryset_list=queryset_list.filter(state__iexact=state)                  
+     #Bedroom
+    if 'bedrooms' in request.GET:
+        bedrooms= request.GET['bedrooms']
+        if bedrooms:
+            queryset_list=queryset_list.filter(bedrooms__lte=bedrooms) 
+     #price
+    if 'price' in request.GET:
+        price= request.GET['price']
+        if price:
+            queryset_list=queryset_list.filter(price__lte=price)                 
+    context = {
+        
+        'state_choices':state_choices,    
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html',context)
 
