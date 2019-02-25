f7a2129544aabc703691cd351e71c46626a7cd3b
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-02-25 22:02:06 +0530

    Add accounts urls in mail url

--- djangofirst/urls.py
+++ djangofirst/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('listings/',include('listings.urls')),
+    path('accounts/',include('accounts.url')),
     path('',include('pages.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
