575332d727c975e1ae8d4ffc6af8d7f420864652
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-03-12 21:57:35 +0530

    add alert functionalities

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        # Register User
+        messages.error(request,'Testing error message')
         return redirect('register')
     else:    
         return render(request,'accounts/register.html')
--- djangofirst/settings.py
+++ djangofirst/settings.py
@@ -134,3 +134,10 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+    
+}
