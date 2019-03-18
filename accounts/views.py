a7bd70a402720fe75fac6fcf33a703da1c6868fd
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-03-18 22:50:47 +0530

    Add login validations

--- accounts/views.py
+++ accounts/views.py
@@ -35,6 +35,16 @@ def register(request):
         return render(request,'accounts/register.html')
 def login(request):
     if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')    
+            return redirect('login')
         # Login User
         return redirect('login')
     else:    
