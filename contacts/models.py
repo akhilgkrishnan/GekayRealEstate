75e2dccb7e59aced6161dee53d41cae8d134ce50
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-03-27 20:45:03 +0530

    add new model data for enquiry

--- contacts/models.py
+++ contacts/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
-    contact_data = models.DateTimeField(default=datetime.now, blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
     def __str__(self):
         return self.name
