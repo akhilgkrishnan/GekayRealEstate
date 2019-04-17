4ae48c0b5e8e81bc5aac920ae433ca670c2aadd5
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-04-17 20:24:17 +0530

    add heroku deploy

--- djangofirst/settings.py
+++ djangofirst/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -149,3 +149,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER ='akhilgk4u@gmail.com'
 EMAIL_HOST_PASSWORD='opcvdcudqefkxfby'
 EMAIL_USE_TLS = True
+
+#Activate Django-Heroku
+
+django_heroku.settings(locals())
